fa550df0ebaa25a9e043291910c6412b9d5d9c25
thegenzdev <thegenzdev@gmail.com>
2023-03-17 02:38:48 -0700

    101 advanced task error space test2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,27 +3,47 @@
 /**
  * main - prints all possible different combinations of three digits
  * Return: Always 0 (Success)
+ *
  */
 int main(void)
 {
-int a, b, c, d;
-for (a = 0; a < 1000; a++)
-{
-b = a / 100;
-c = (a / 10) % 10;
-d = a % 10; 
-if (b < c && c < d)
-{
-putchar(b + '0');
-putchar(c + '0');
-putchar(d + '0');
-if (a < 700)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
-return (0);
+	int digit1;
+	int digit2;
+	int digit3;
+
+	digit1 = 48;
+	digit2 = 49;
+	digit3 = 50;
+	while  ((digit1 < 56) && (digit2 < 57) && (digit3 < 58))
+	{
+		putchar(digit1);
+		putchar(digit2);
+		putchar(digit3);
+		if ((digit1 == 55) && (digit2 == 56) && (digit3 == 57))
+		{
+			putchar('\n');
+			digit1++;
+			digit2++;
+			digit3++;
+		}
+		else
+		{
+			putchar(44);
+			putchar(32);
+			if (digit3 < 57)
+			{
+				digit3++;
+			}
+			else if ((digit2 < 56) && (digit3 == 57))
+			{
+				digit3 = 1 + ++digit2;
+			}
+			else
+			{
+				digit2 = 1 + ++digit1;
+				digit3 = 1 + digit2;
+			}
+		}
+	}
+	return (0);
 }
