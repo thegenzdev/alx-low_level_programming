9d42a8266539f6b38ef9892e5f450f7e340d5e24
thegenzdev <thegenzdev@gmail.com>
2023-04-27 07:18:35 -0700

    testing

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,10 +1,13 @@
+#include <stdio.h>
 #include "lists.h"
+
 /**
  * print_list - prints list.
  * @h: param list
  * Return: struct
  */
-size_t print_list(const list_t *h)
+
+size_t print_list(const list_t *h);
 {
 	size_t nodelen;
 
