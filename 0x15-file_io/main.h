a85beab2693bf0f67271fb47d122a4396f4ddb6d
thegenzdev <thegenzdev@gmail.com>
2023-05-08 07:03:20 -0700

    advanced task

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,13 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <elf.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <elf.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
